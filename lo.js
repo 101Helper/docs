d15e4d2b1fa38f035dfa9486a948eafef83871fa
fahimraza <fahimraza101@gmail.com>
2016-06-25 12:23:51 +0500

    Delete lo.js

--- lo.js
+++
@@ -1,7 +0,0 @@
-var a = document.createElement('a');
-var linkText = document.createTextNode("101helper");
-a.appendChild(linkText);
-a.title = "101helper";
-a.setAttribute("rel", "dofollow");
-a.href = "http://101helper.blogspot.com";
-document.body.appendChild(a);
