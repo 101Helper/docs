c90294f5254ae6c76db4bec3e78eb2eecfcb20e6
fahimraza <fahimraza101@gmail.com>
2016-06-25 12:06:36 +0500

    Create 101helpercode.js

---
+++ 101helpercode.js
@@ -0,0 +1,7 @@
+var a = document.createElement('a');
+var linkText = document.createTextNode("101helper");
+a.appendChild(linkText);
+a.title = "101helper";
+a..attr('rel','dofollow')
+a.href = "101helper.blogspot.com";
+document.body.appendChild(a);
