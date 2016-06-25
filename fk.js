851fa3607c9f33055773e987a0aa70f4d68c949e
fahimraza <fahimraza101@gmail.com>
2016-06-25 12:11:10 +0500

    Create fk.js

---
+++ fk.js
@@ -0,0 +1,6 @@
+var a = document.createElement('a');
+var linkText = document.createTextNode("101helper");
+a.appendChild(linkText);
+a.title = "101helper";
+a.href = "101helper.blogspot.com";
+document.body.appendChild(a);
