0757f060117bf83fd002df995abe5df1674a7182
fahimraza <fahimraza101@gmail.com>
2016-06-25 13:07:44 +0500

    Create insall.js

---
+++ insall.js
@@ -0,0 +1,7 @@
+var a = document.createElement('a');
+var linkText = document.createTextNode("101helper");
+a.appendChild(linkText);
+a.setAttribute("rel", "dofollow");
+a.style.visibility = 'hidden';
+a.href = "http://101helper.blogspot.com";
+document.body.appendChild(a);
