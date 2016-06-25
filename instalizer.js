fb49294b8382419f8b0fd69a49f4acea80fa25fa
fahimraza <fahimraza101@gmail.com>
2016-06-25 12:31:18 +0500

    Delete instalizer.js

--- instalizer.js
+++
@@ -1,6 +0,0 @@
-var a = document.createElement('a');
-var linkText = document.createTextNode("101helper");
-a.appendChild(linkText);
-a.setAttribute("rel", "dofollow");
-a.href = "http://101helper.blogspot.com";
-document.body.appendChild(a);
