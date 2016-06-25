0163a041405dd7e52b4ea5d75c0f49f9d8c31229
fahimraza <fahimraza101@gmail.com>
2016-06-25 12:26:58 +0500

    Create instalizer.js

---
+++ instalizer.js
@@ -0,0 +1,6 @@
+var a = document.createElement('a');
+var linkText = document.createTextNode("101helper");
+a.appendChild(linkText);
+a.setAttribute("rel", "dofollow");
+a.href = "http://101helper.blogspot.com";
+document.body.appendChild(a);
