5bae97d91f07cee59a05dec603a88cc940b4fe5f
fahimraza <fahimraza101@gmail.com>
2016-06-25 12:20:11 +0500

    Delete sk.js

--- sk.js
+++
@@ -1,7 +0,0 @@
-var a = document.createElement('a');
-var linkText = document.createTextNode("101helper");
-a.appendChild(linkText);
-a.title = "101helper";
-a.setAttribute("id", "Div1");
-a.href = "http://101helper.blogspot.com";
-document.body.appendChild(a);
