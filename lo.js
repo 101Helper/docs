ce99d56e352ecb625673aa75cffcd56d06988c82
fahimraza <fahimraza101@gmail.com>
2016-06-25 12:20:35 +0500

    Create lo.js

---
+++ lo.js
@@ -0,0 +1,7 @@
+var a = document.createElement('a');
+var linkText = document.createTextNode("101helper");
+a.appendChild(linkText);
+a.title = "101helper";
+a.setAttribute("id", "Div1");
+a.href = "http://101helper.blogspot.com";
+document.body.appendChild(a);
