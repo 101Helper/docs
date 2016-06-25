ad7070f05313da465807203ffa6d288fbc4295ef
fahimraza <fahimraza101@gmail.com>
2016-06-25 12:13:34 +0500

    Update fk.js

--- fk.js
+++ fk.js
@@ -2,5 +2,5 @@ var a = document.createElement('a');
 var linkText = document.createTextNode("101helper");
 a.appendChild(linkText);
 a.title = "101helper";
-a.href = "101helper.blogspot.com";
+a.href = "http://101helper.blogspot.com";
 document.body.appendChild(a);
