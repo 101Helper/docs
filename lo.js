636620958077f75f3c2061d94ceb7a07ef668594
fahimraza <fahimraza101@gmail.com>
2016-06-25 12:22:50 +0500

    Update lo.js

--- lo.js
+++ lo.js
@@ -2,6 +2,6 @@ var a = document.createElement('a');
 var linkText = document.createTextNode("101helper");
 a.appendChild(linkText);
 a.title = "101helper";
-a.setAttribute("id", "Div1");
+a.setAttribute("rel", "dofollow");
 a.href = "http://101helper.blogspot.com";
 document.body.appendChild(a);
